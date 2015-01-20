d01fad05168a5f519bf236356c8ab65e56d8603f
houyumde <55366762@qq.com>
2015-01-20 16:14:21 +0800

    update

--- src/com/yc/entity/user/Department.java
+++ src/com/yc/entity/user/Department.java
@@ -1,9 +1,35 @@
 package com.yc.entity.user;
 
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+@DiscriminatorValue("department")//职位
 public class Department {
 	
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;//部门ID
+    
+    @Column (length=32)
 	private String departmentname;//部门名称
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getDepartmentname() {
+		return departmentname;
+	}
+	public void setDepartmentname(String departmentname) {
+		this.departmentname = departmentname;
+	}
 
 
 }
