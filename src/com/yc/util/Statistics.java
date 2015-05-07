184af8ba962b3d12a252745ef03bee98c3d099c1
jiangbo12345 <251851509@qq.com>
2015-05-07 19:29:01 +0800

    update

--- src/com/yc/util/Statistics.java
+++ src/com/yc/util/Statistics.java
@@ -2,7 +2,10 @@ package com.yc.util;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -14,13 +17,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 import net.sf.json.JsonConfig;
 import net.sf.json.util.CycleDetectionStrategy;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.yc.entity.user.DepartAndPositions;
 import com.yc.entity.user.Department;
@@ -34,9 +40,8 @@ import com.yc.service.IPersonnelService;
 @Controller
 @RequestMapping("/analysis")
 public class Statistics {
-	@Autowired
-	IPersonnelService perService;
-
+	private static final int FIRST_DAY = Calendar.MONDAY;
+	
 	@Autowired
 	IDepartAndPositionsService depAndPositionsService;
 
@@ -47,10 +52,11 @@ public class Statistics {
 	IDepartmentService departmentService;
 
 	@RequestMapping(value = "removecauses", method = RequestMethod.POST)
-	public void removecauses(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		JsonConfig jsonConfig = new JsonConfig();
+	@ResponseBody
+	public Map<String, Object> removecauses(String param,HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		Personnel personnel = (Personnel) request.getSession().getAttribute("loginPersonnle");
-		jsonConfig.setCycleDetectionStrategy(CycleDetectionStrategy.LENIENT);
+		ModelMap mode = new ModelMap();
+		Map<String, List<PersonnelStatistics>> map  = new HashMap<String, List<PersonnelStatistics>>();
 		if (personnel.getDepartAndPositions() != null) {
 			List<Personnel> personnels = new ArrayList<Personnel>();
 			if (personnel.getDepartAndPositions().getDepartment().getDepartmentID() == 2) {
@@ -79,22 +85,87 @@ public class Statistics {
 					personnels.addAll(personne);
 				}
 			}
-			List<PersonnelStatistics> arryList = new ArrayList<PersonnelStatistics>();
-			Map<String, List<PersonnelStatistics>> map  = new HashMap<String, List<PersonnelStatistics>>();
-			for (int i = 0; i < personnels.size(); i++) {
-				PersonnelStatistics comm = new PersonnelStatistics();
-				comm.setUserName(personnels.get(i).getUserName());
-				comm.setNum(personnels.get(i).getId());
-				arryList.add(comm);
+			String days = "";
+			if (param!= null && param.equals("week")) {
+				days = printWeekdays("week",7);
+			}else if (param!= null && param.equals("months")) {
+				days = printMonths(getFirstDayOfMonth(new Date()),new Date());
+			}else{
+				days = printMonths(getFirstDayOfYear(new Date()),new Date());
 			}
-			map.put("jiangbo", arryList);
-			JSONArray json = JSONArray.fromObject(arryList, jsonConfig);
-			System.out.println(json.toString());
-			response.setContentType("application/json;charset=UTF-8");
-			PrintWriter out = response.getWriter();
-			out.println(json);
-			out.flush();
-			out.close();
+			List<PersonnelStatistics> arryList = null;
+			for (int i = 0; i < personnels.size(); i++) {
+				arryList = personnelService.getOrtherByParam(days,personnels.get(i));
+				map.put(personnels.get(i).getUserName(), arryList);
+			}
+			mode.put("statistics", map);
+			days = days.replace("'", "");
+			mode.put("days", days);
 		}
+		return mode;
 	}
+	 
+    private static String printWeekdays(String week,Integer days) {
+    	StringBuffer sb = new StringBuffer();
+        Calendar calendar = Calendar.getInstance();
+        setToFirstDay(calendar);
+        for (int i = 0; i < days; i++) {
+        	sb.append("'"+printDay(calendar)+"',");
+            calendar.add(Calendar.DATE, 1);
+        }
+        return sb.substring(0, sb.length()-1);
+    }
+ 
+    private static void setToFirstDay(Calendar calendar) {
+        while (calendar.get(Calendar.DAY_OF_WEEK) != FIRST_DAY) {
+            calendar.add(Calendar.DATE, -1);
+        }
+    }
+ 
+    private static String printDay(Calendar calendar) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        return dateFormat.format(calendar.getTime());
+    }
+    public static String printMonths(Date dBegin, Date dEnd) {  
+    	SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        StringBuffer sb = new StringBuffer("'"+dateFormat.format(dBegin)+"',");
+        Calendar calBegin = Calendar.getInstance();  
+        // 使用给定的 Date 设置此 Calendar 的时间    
+        calBegin.setTime(dBegin);  
+        Calendar calEnd = Calendar.getInstance();  
+        // 使用给定的 Date 设置此 Calendar 的时间    
+        calEnd.setTime(dEnd);  
+        // 测试此日期是否在指定日期之后    
+        while (dEnd.after(calBegin.getTime())) {  
+            // 根据日历的规则，为给定的日历字段添加或减去指定的时间量    
+            calBegin.add(Calendar.DAY_OF_MONTH, 1);  
+            sb.append(dateFormat.format("'"+calBegin.getTime())+"',");  
+        }  
+        return  sb.substring(0, sb.length()-1);
+    }
+    /** 
+     * 得到本月第一天的日期 
+     * @Methods Name getFirstDayOfMonth 
+     * @return Date 
+     */  
+    public Date getFirstDayOfMonth(Date date)   {     
+        Calendar cDay = Calendar.getInstance();     
+        cDay.setTime(date);  
+        cDay.set(Calendar.DAY_OF_MONTH, 1);  
+        System.out.println(cDay.getTime());  
+        return cDay.getTime();     
+    }  
+    /** 
+     * 得到本年第一天的日期 
+     * @Methods Name getFirstDayOfMonth 
+     * @return Date 
+     */  
+    public Date getFirstDayOfYear(Date date)   {     
+    	Calendar cDay = Calendar.getInstance();     
+    	cDay.setTime(date);  
+    	cDay.set(Calendar.DAY_OF_YEAR, 1);  
+    	System.out.println(cDay.getTime());  
+    	return cDay.getTime();     
+    }  
+    
 }
